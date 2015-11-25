645ae8b510ae59043844c40860439b1ad7707940
Dan Nguyen <dansonguyen@gmail.com>
2015-11-25 13:03:19 -0600

    data

--- config.rb
+++ config.rb
@@ -37,14 +37,14 @@ end
 
 
 activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = '2015.padjo.org' # The name of the S3 bucket you are targetting. This is globally unique.
+#  s3_sync.bucket                     = '2015.padjo.org' # The name of the S3 bucket you are targetting. This is globally unique.
   s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
   s3_sync.delete                     = false # We delete stray files by default.
-  s3_sync.after_build                = true # We do not chain after the build step by default.
+#  s3_sync.after_build                = true # We do not chain after the build step by default.
   s3_sync.prefer_gzip                = false
   s3_sync.path_style                 = true
   s3_sync.reduced_redundancy_storage = false
   s3_sync.acl                        = 'public-read'
   s3_sync.encryption                 = false
-  s3_sync.prefix                     = ''
+#  s3_sync.prefix                     = ''
 end
